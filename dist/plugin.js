a4067a1e95528b1ac3fe4df6fa258203d30e507a
Ankit Singhania <ankit.s@chalo.com>
2026-06-29 13:25:29 +0530

    Commented code for testing

--- dist/plugin.js
+++ dist/plugin.js
@@ -9,16 +9,25 @@ var capacitorCapacitorGoogleMaps = (function (exports, core, markerclusterer) {
         const x = data.x;
         const y = data.y;
         const elem = document.elementFromPoint(x, y);
-        const mapElement = Array.from(document.querySelectorAll("[data-internal-id]")).find((el) => el.dataset.internalId === data.mapId);
-        const mapElementAtPoint = elem === null || elem === void 0 ? void 0 : elem.closest("[data-internal-id]");
-        const interactiveElementAtPoint = elem === null || elem === void 0 ? void 0 : elem.closest('a,button,input,textarea,select,[role="button"],[data-map-control="true"]');
-        const pointInsideMap = mapElement &&
-            (() => {
-                const rect = mapElement.getBoundingClientRect();
-                return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
-            })();
-        const mapInFocus = Boolean(((_a = mapElementAtPoint === null || mapElementAtPoint === void 0 ? void 0 : mapElementAtPoint.dataset) === null || _a === void 0 ? void 0 : _a.internalId) === data.mapId ||
-            (pointInsideMap && !interactiveElementAtPoint));
+        const internalId = (_a = elem === null || elem === void 0 ? void 0 : elem.dataset) === null || _a === void 0 ? void 0 : _a.internalId;
+        const mapInFocus = internalId === data.mapId;
+        // const mapElement = Array.from(
+        //   document.querySelectorAll<HTMLElement>("[data-internal-id]")
+        // ).find((el) => el.dataset.internalId === data.mapId);
+        // const mapElementAtPoint = elem?.closest<HTMLElement>("[data-internal-id]");
+        // const interactiveElementAtPoint = elem?.closest(
+        //   'a,button,input,textarea,select,[role="button"],[data-map-control="true"]'
+        // );
+        // const pointInsideMap =
+        //   mapElement &&
+        //   (() => {
+        //     const rect = mapElement.getBoundingClientRect();
+        //     return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+        //   })();
+        // const mapInFocus = Boolean(
+        //   mapElementAtPoint?.dataset?.internalId === data.mapId ||
+        //     (pointInsideMap && !interactiveElementAtPoint)
+        // );
         CapacitorGoogleMaps.dispatchMapEvent({ id: data.mapId, focus: mapInFocus });
     });
 
